daa40cc7c3d4a6f9c5effab3fc9e48dd4f107b81
Valiantsin Haradkou <valicser@yandex.ru>
2017-04-08 18:53:57 +0300

    changed

--- bot/app.js
+++ bot/app.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 8080, function () {
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "57c6575d-2095-408d-804d-2fead599c7b0",
-    appPassword: "5rHdUcMQwq1E977iCCNOGkG"
+    appId: "420370ab-6e82-4287-af5d-3920fbfd59f7",
+    appPassword: "URDKo7QkbQXkm6FFqCk4nb9"
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
