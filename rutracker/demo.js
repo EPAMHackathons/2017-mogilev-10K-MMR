829a77cfc71e29811ede02c41812f647b1d290c7
Pitlis <pitlis95@gmail.com>
2017-04-08 07:55:53 +0000

    demo for rutracker

---
+++ rutracker/demo.js
@@ -0,0 +1,24 @@
+let getTorrents = require('./rutracker').getTorrents;
+
+
+getTorrents("Социальная сеть", 2010, "Финчер").then(res => {
+    console.log(res);
+})
+
+// [{
+//         quality: 'BDRip',
+//         data: {
+//             size: '1.56 GB',
+//             url: 'http://rutracker.org/forum/viewtopic.php?t=4368278',
+//             seeds: '61'
+//         }
+//     },
+//     {
+//         quality: 'HDRip',
+//         data: {
+//             size: '1.56 GB',
+//             url: 'http://rutracker.org/forum/viewtopic.php?t=3318235',
+//             seeds: '4'
+//         }
+//     }
+// ]
